c02f643f36c1683c31c39bbdca2bcd208f2f8143
Hans Dijkema <hans@dijkewijk.nl>
2024-06-24 15:04:41 +0200

    inst

--- install.sh
+++ install.sh
@@ -31,6 +31,26 @@ mkdir -p $BASE/run
 echo "Installing libraries"
 echo "---------------------------------------------------------------"
 
+# Download JCorporation's signing key locally and install it in a dedicated keyring
+curl http://download.opensuse.org/repositories/home:/jcorporation/Debian_11/Release.key | gpg --no-default-keyring --keyring /usr/share/keyrings/jcorporation.github.io.gpg --import
+
+# ⚠️ VERIFY the fingerprint of the downloaded key (A37A ADC4 0A1C C6BE FB75  372F AA09 B8CC E895 BD7D - home:jcorporation OBS Project <home:jcorporation@build.opensuse.org>) 
+gpg --no-default-keyring --keyring /usr/share/keyrings/jcorporation.github.io.gpg --fingerprint
+
+# Make the imported keyring world-readable
+chmod 644 /usr/share/keyrings/jcorporation.github.io.gpg
+
+# Get Debian VERSION_ID from os-release file
+source /etc/os-release
+echo $VERSION_ID
+
+# Add JCorporation APT repository and ensure releases are signed with the repository's official keys
+cat <<EOF > /etc/apt/sources.list.d/jcorporation.list
+deb [signed-by=/usr/share/keyrings/jcorporation.github.io.gpg] http://download.opensuse.org/repositories/home:/jcorporation/Debian_$VERSION_ID/ ./
+EOF
+cat /etc/apt/sources.list.d/jcorporation.list
+
+apt-get -y update
 apt-get -y install git \
                    libao-dev libasound2-dev libc6-dev libcrypt-dev libexpat1-dev libffi-dev \
                    libflac-dev libgcc-12-dev libicu-dev libncurses-dev libnsl-dev libogg-dev \
