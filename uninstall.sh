be4130ca9c7e8496cbe061e56f26188c1e04393e
Hans Dijkema <hans@dijkewijk.nl>
2024-06-24 14:34:12 +0200

    ja

--- uninstall.sh
+++ uninstall.sh
@@ -3,9 +3,21 @@
 BASE=/opt/music
 
 echo ""
+echo "Stopping and disabling services"
+systemctl stop mympd
+systemctl stop mpd
+systemctl stop mpd.socket
+systemctl stop spotify-connect
+systemctl disable mympd
+systemctl disable mpd
+systemctl disable mpd.socket
+systemctl disable spotify-connect
+
+echo ""
 echo "Removing installation from $BASE"
 
 rm -rf $BASE
+rm -rf src/*
 
 echo ""
 echo "done"
