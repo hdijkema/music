69de72af750610579f03728f284c584afcb89e76
Hans Dijkema <hans@dijkewijk.nl>
2024-06-24 16:58:39 +0200

    ja

--- install.sh
+++ install.sh
@@ -42,24 +42,29 @@ mkdir -p $BASE/run
 echo "Installing libraries"
 echo "---------------------------------------------------------------"
 
-# Download JCorporation's signing key locally and install it in a dedicated keyring
-curl http://download.opensuse.org/repositories/home:/jcorporation/Debian_11/Release.key | gpg --no-default-keyring --keyring /usr/share/keyrings/jcorporation.github.io.gpg --import
+apt-get install aptitude
 
-# ⚠️ VERIFY the fingerprint of the downloaded key (A37A ADC4 0A1C C6BE FB75  372F AA09 B8CC E895 BD7D - home:jcorporation OBS Project <home:jcorporation@build.opensuse.org>) 
-gpg --no-default-keyring --keyring /usr/share/keyrings/jcorporation.github.io.gpg --fingerprint
+MYMPD=`aptitude search mympd`
+if [ "$MYMPD" != "" ]; then
+  # Download JCorporation's signing key locally and install it in a dedicated keyring
+  curl http://download.opensuse.org/repositories/home:/jcorporation/Debian_11/Release.key | gpg --no-default-keyring --keyring /usr/share/keyrings/jcorporation.github.io.gpg --import
 
-# Make the imported keyring world-readable
-chmod 644 /usr/share/keyrings/jcorporation.github.io.gpg
+  # ⚠️ VERIFY the fingerprint of the downloaded key (A37A ADC4 0A1C C6BE FB75  372F AA09 B8CC E895 BD7D - home:jcorporation OBS Project <home:jcorporation@build.opensuse.org>) 
+  gpg --no-default-keyring --keyring /usr/share/keyrings/jcorporation.github.io.gpg --fingerprint
 
-# Get Debian VERSION_ID from os-release file
-source /etc/os-release
-echo $VERSION_ID
+  # Make the imported keyring world-readable
+  chmod 644 /usr/share/keyrings/jcorporation.github.io.gpg
 
-# Add JCorporation APT repository and ensure releases are signed with the repository's official keys
-cat <<EOF > /etc/apt/sources.list.d/jcorporation.list
+  # Get Debian VERSION_ID from os-release file
+  source /etc/os-release
+  echo $VERSION_ID
+
+  # Add JCorporation APT repository and ensure releases are signed with the repository's official keys
+  cat <<EOF > /etc/apt/sources.list.d/jcorporation.list
 deb [signed-by=/usr/share/keyrings/jcorporation.github.io.gpg] http://download.opensuse.org/repositories/home:/jcorporation/Debian_$VERSION_ID/ ./
 EOF
-cat /etc/apt/sources.list.d/jcorporation.list
+  cat /etc/apt/sources.list.d/jcorporation.list
+fi
 
 apt-get -y update
 apt-get -y install git \
